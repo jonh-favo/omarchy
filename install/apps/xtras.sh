7c806a41ab893ed412314a54816e4c3b7915b903
Jon Håvard Gundersen <jonh@favo.no>
2025-08-11 22:38:21 +0200

    Added tweaks necessary to install on cachyos

--- install/apps/xtras.sh
+++ install/apps/xtras.sh
@@ -12,8 +12,7 @@ if [ -z "$OMARCHY_BARE" ]; then
       echo -e "\e[31mFailed to install $pkg. Continuing without!\e[0m"
   done
 
-  yay -S --noconfirm --needed 1password-beta 1password-cli ||
-    echo -e "\e[31mFailed to install 1password. Continuing without!\e[0m"
+  #yay -S --noconfirm --needed 1password-beta 1password-cli || echo -e "\e[31mFailed to install 1password. Continuing without!\e[0m"
 fi
 
 # Copy over Omarchy applications
--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -4,5 +4,5 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
   fd eza fzf ripgrep zoxide bat jq xmlstarlet \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate bash-completion \
+  man  less whois plocate bash-completion \
   alacritty
